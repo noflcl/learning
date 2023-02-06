16d916d1dcc1c47470675f86788c4d8147f9d587
Stephen McNamara <123384352+noflcl@users.noreply.github.com>
2023-02-05 20:20:16 -0700

    Final solution for alert box

---
+++ javascript/stackOverflow/questions75024202.js
@@ -0,0 +1,7 @@
+// When the user clicks, open the popup
+function popupFunction() {
+    // get the HTML element "myPopup" which is defined in your span element
+    let popup = document.getElementById("myPopup");
+    // toggle visibile 
+    popup.classList.toggle("show");
+}
