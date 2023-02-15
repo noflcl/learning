53e0f8987f8980c170d79d2c7c15f10f676c4db1
Stephen McNamara <123384352+noflcl@users.noreply.github.com>
2023-02-15 04:53:56 -0700

    Moved
    Moved to new org system

--- javascript/stackOverflow/README.md
+++
@@ -1,7 +0,0 @@
-# Question 75024202
-
-## Alert box that works with Google Pages
-
-![alt text](questions75024202.png)
-
-The final solution uses a overlay to darken the screen, and a box centers at the top with your alert.
--- javascript/stackOverflow/questions75024202.js
+++
@@ -1,7 +0,0 @@
-// When the user clicks, open the popup
-function popupFunction() {
-    // get the HTML element "myPopup" which is defined in your span element
-    let popup = document.getElementById("myPopup");
-    // toggle visibile 
-    popup.classList.toggle("show");
-}
